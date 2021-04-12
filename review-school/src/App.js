622e958b7352a3944782d4658b035c071f36ca9e
MinhNguyenLe <18520100@gm.uit.edu.vn>
2021-04-12 12:01:48 +0700

    dowload-react-bootstrap

--- review-school/src/App.js
+++ review-school/src/App.js
@@ -3,10 +3,13 @@ import { Route, Switch} from "react-router-dom";
 import Navbar from "./components/Navbar.js"
 import Footer from "./components/Footer.js"
 
+import {Button} from 'react-bootstrap';
+
 function App() {
   return (
     <div>
       <Navbar></Navbar>
+      <Button>Test</Button>
       <Footer></Footer>
     </div>  
   );
