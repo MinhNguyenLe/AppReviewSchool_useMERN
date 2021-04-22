cbfdac29564bb3732ae426f8b1b8df833fbd8a0b
MinhNguyenLe <18520100@gm.uit.edu.vn>
2021-04-22 15:08:12 +0700

    update-redux-thunk-2

--- review-school/src/App.js
+++ review-school/src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
 import { Route, Switch} from "react-router-dom";
+import Review from "./pages/Review.js"
 import Navbar from "./components/Navbar.js"
 import Footer from "./components/Footer.js"
-
-import {Button} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
-      <Button>Test</Button>
+      <Switch>
+        <Route exact path="/review/nameSchool" component={Review}></Route>
+      </Switch>
       <Footer></Footer>
     </div>  
   );
