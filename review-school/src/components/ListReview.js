90d31beb69265b8f69c40c1fae7fa19111fd6eb7
MinhNguyenLe <18520100@gm.uit.edu.vn>
2021-05-04 14:23:12 +0700

    axios data for schools and reviews

--- review-school/src/components/ListReview.js
+++ review-school/src/components/ListReview.js
@@ -1,36 +1,49 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import * as rb  from 'react-bootstrap';
 import "./ListReview.css";
+import axios from 'axios';
 
 const ListReview =()=>{
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const axiosData = async () => {
+      const result = await axios.get(
+        'http://localhost:9000/api/reviews',
+      );
+      setData(result.data);
+    };
+    axiosData();
+  }, []); 
   return(
     <div>
-      <rb.Card>
+    {data.map((item,index) => (
+      <rb.Card key={index}>
         <div className="d-flex flex-row align-items-center">
           <div className="icon-user d-flex align-items-center justify-content-center"> 
             <i className="fas fa-user"></i>
             </div>
           <div>
-            <rb.Card.Text>Người dùng OurApp</rb.Card.Text>
-            <rb.Card.Text>Ngày 20-04-2021</rb.Card.Text>
+            <rb.Card.Text>Người dùng App</rb.Card.Text>
+            <rb.Card.Text>{item.createdAt}</rb.Card.Text>
           </div>
         </div>
         <rb.Card.Text className="review-name-school">Ngoại Thương _ Clc Tiếng Anh</rb.Card.Text>
         <rb.Card.Text className="review-content">Đã học khoá học: Ngoại Thương tại đây.</rb.Card.Text>
         <div>
           <rb.Card.Text className="review-title">Ưu điểm</rb.Card.Text>
-          <rb.Card.Text className="review-content">-Cơ sở vật chất rất tốt, hiện đại. Âm thanh, ánh sáng, wifi, không gian học tập phù hợp với nhu cầu sinh viên. Khi gặp vấn đề kỹ thuật luôn có đội ngũ nhân viên hỗ trợ kịp thời</rb.Card.Text>
-          <rb.Card.Text className="review-content">-Giảng viên ở các môn chuyên ngành có nhiều kinh nghiệm, luôn cập nhật kiến thức mới và cách giảng dạy cho phù hợp với nhu cầu sinh viên</rb.Card.Text>
+          <rb.Card.Text className="review-content">-{item.positive}</rb.Card.Text>
         </div>
         <div>
           <rb.Card.Text className="review-title">Điểm cần cải thiện</rb.Card.Text>
-          <rb.Card.Text className="review-content">-Một vài giảng viên ở các môn đại cương có thể tăng cường các hoạt động tương tác, kết hợp giữa việc tự học và giảng giải tại lớp song song để đem lại trải nghiệm tốt nhất thay vì việc giảng dạy chỉ đến từ 1 phía</rb.Card.Text>
+          <rb.Card.Text className="review-content">-{item.negative}</rb.Card.Text>
         </div>
         <div>
           <rb.Card.Text className="review-title">Trải nghiệm và lời khuyên</rb.Card.Text>
           <rb.Card.Text className="review-content">-Bạn cần phải có tinh thần tự học cao, luôn tìm hiểu cái mới. Đọc tin tức, sách báo nhiều hơn để phù hợp với tính chất ngành học</rb.Card.Text>
         </div>
         </rb.Card>
+    ))}
     </div>
   )
 }
--- review-school/src/components/ListSchool.js
+++ review-school/src/components/ListSchool.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import * as rb  from 'react-bootstrap';
 import "./ListSchool.css";
+import axios from 'axios';
 
 const ListSchool =()=>{
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const axiosData = async () => {
+      const result = await axios.get(
+        'http://localhost:9000/api/schools',
+      );
+      setData(result.data);
+    };
+    axiosData();
+  }, []); 
+
   return(
     <div>
-      <rb.Card>
+    {data.map((item,index) => (
+      <rb.Card key={index} >
         <rb.Card.Link href="/review/nameSchool" className="d-flex flex-row">
           <rb.Card.Img src="https://edu2review.com/upload/school-images/truong-dai-hoc-kinh-te-luat/img/792w-1-truong-dai-hoc-kinh-te-luat.jpg" className="school-img"></rb.Card.Img>
           <rb.Card.Body>
-            <rb.Card.Text className="school-name">Trường đại học CNTT - ĐHQG</rb.Card.Text>
-            <rb.Card.Text className="school-name">Verified by OurApp</rb.Card.Text>
-            <div className="d-flex flex-row align-items-center">
+              <rb.Card.Text className="school-name">{item.name}-{item.code}</rb.Card.Text>
+              <rb.Card.Text className="school-name">{item.location}</rb.Card.Text>
+              <div className="d-flex flex-row align-items-center">
               <rb.Badge variant="success" className="d-flex justify-content-center align-items-center school-badge">9.0</rb.Badge>
               <div>
-                <rb.Card.Text className="school-name">Tốt</rb.Card.Text>
-                <rb.Card.Text>n đánh giá</rb.Card.Text>
+              <rb.Card.Text className="school-name">Tốt</rb.Card.Text>
+              <rb.Card.Text>n đánh giá</rb.Card.Text>
               </div>
-            </div>
-          </rb.Card.Body>
-        </rb.Card.Link>
-      </rb.Card>
+              </div>
+              </rb.Card.Body>
+              </rb.Card.Link>
+              </rb.Card>
+    ))}
     </div>
   )
 }
