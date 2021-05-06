d2b9fa7b19dc6e420c230df8569040224fb77dc9
MinhNguyenLe <18520100@gm.uit.edu.vn>
2021-05-06 16:31:18 +0700

    design navbar

---
+++ review-school/src/dataContent.js
@@ -0,0 +1,8 @@
+export const data ={
+  majorN : ['khoa hoc ky thuat','Xa hoi - Nhan van','Y duoc','Kinh te'],              // nganh hoc Nav
+  schoolN : ['Tai TP HCM','Tai HN','Tai Da Nang','Khu vuc khac'],             // truong hoc Nav
+  moreN : ['Tin tuc ve web','Lien he hop tac','gop y','Gioi thieu ban be'],               // khac Nav
+  countryF : [],             // ca nuoc Filter
+  universityF : [],         // dai hoc Filter
+  majorF : ['khoa hoc ky thuat','Xa hoi - Nhan van','Y duoc','Kinh te'],        //nganh hoc filter
+}
